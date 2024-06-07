5c9df49078775d204285cffcf04c22d1d853d925
utkarshsinh <usinh002@ucr.edu>
2024-06-07 15:43:40 -0700

    second commit

---
+++ src/main/java/com/javatechie/spring/batch/config/SpringBatchConfig.java
@@ -0,0 +1,101 @@
+package com.javatechie.spring.batch.config;
+
+import com.javatechie.spring.batch.entity.Customer;
+import com.javatechie.spring.batch.repository.CustomerRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.data.RepositoryItemWriter;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.LineMapper;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Configuration
+//@EnableBatchProcessing
+@AllArgsConstructor
+public class SpringBatchConfig {
+
+//    private JobBuilderFactory jobBuilderFactory;
+//
+//    private StepBuilderFactory stepBuilderFactory;
+
+    private CustomerRepository customerRepository;
+
+
+    @Bean
+    public FlatFileItemReader<Customer> reader() {
+        FlatFileItemReader<Customer> itemReader = new FlatFileItemReader<>();
+        itemReader.setResource(new FileSystemResource("src/main/resources/customers.csv"));
+        itemReader.setName("csvReader");
+        itemReader.setLinesToSkip(1);
+        itemReader.setLineMapper(lineMapper());
+        return itemReader;
+    }
+
+    private LineMapper<Customer> lineMapper() {
+        DefaultLineMapper<Customer> lineMapper = new DefaultLineMapper<>();
+// LineTokenizer will extract the values from CSV file
+        DelimitedLineTokenizer lineTokenizer = new DelimitedLineTokenizer();
+        lineTokenizer.setDelimiter(",");
+        lineTokenizer.setStrict(false);
+        lineTokenizer.setNames("id", "firstName", "lastName", "email", "gender", "contactNo", "country", "dob");
+// Field set mapper will map the values to the target class
+        BeanWrapperFieldSetMapper<Customer> fieldSetMapper = new BeanWrapperFieldSetMapper<>();
+        fieldSetMapper.setTargetType(Customer.class);
+// Now both the objects we will provide to lineMapper
+        lineMapper.setLineTokenizer(lineTokenizer);
+        lineMapper.setFieldSetMapper(fieldSetMapper);
+        return lineMapper;
+
+    }
+
+    @Bean
+    public CustomerProcessor processor() {
+        return new CustomerProcessor();
+    }
+
+    @Bean
+    public RepositoryItemWriter<Customer> writer() {
+        RepositoryItemWriter<Customer> writer = new RepositoryItemWriter<>();
+        writer.setRepository(customerRepository);
+        writer.setMethodName("save");
+        return writer;
+    }
+
+    @Bean
+    public Step step1(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+        return new StepBuilder("csv-step", jobRepository )
+                .<Customer, Customer>chunk(10, transactionManager)
+                .reader(reader())
+                .processor(processor())
+                .writer(writer())
+                .taskExecutor(taskExecutor())
+                .build();
+    }
+
+    @Bean
+    public Job runJob(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+        return new JobBuilder("importCustomers", jobRepository)
+                .flow(step1(jobRepository, transactionManager)).end().build();
+
+    }
+
+    @Bean
+    public TaskExecutor taskExecutor() {
+        SimpleAsyncTaskExecutor asyncTaskExecutor = new SimpleAsyncTaskExecutor();
+        asyncTaskExecutor.setConcurrencyLimit(10);
+        return asyncTaskExecutor;
+    }
+
+}
